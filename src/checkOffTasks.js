b9fda1c08dc9063904a4127f59d5c94129560994
Erika <erikavarghese@gmail.com>
2022-12-04 20:15:19 -0500

    fixed bugs for cal view delete + cal view check off

--- src/checkOffTasks.js
+++ src/checkOffTasks.js
@@ -1,10 +1,35 @@
 import arrayToDisplay from "./arrayToDisplay";
+import storeDatedTasks from "./storeDatedTasks";
+import storeTasks from "./storeTasks";
 
 function checkOffTasks(x) {
-    let check = document.querySelector(".checkOffTaskBtn");
-    check.addEventListener("click", () => setInterval(function() {
-        arrayToDisplay(x);
-    }, 100));
+    document.addEventListener("click", function(e) {
+        const target = e.target.closest(".checkOffTaskBtn");
+        if(target){
+            if(x===storeDatedTasks) {
+                // loop through storeDatedTasks and storeTasks
+                // find objects with common title but not common done property value
+                
+                let arr1 = storeTasks.filter(o => storeDatedTasks.some(({title, done}) => o.title === title && o.done !== done));
+                console.table(arr1);
+
+                //replace storeTasks done value with storeDatedTasks done value
+                // 1) find where storeTasks title === arr1 title
+                // 2) storeTasks[index].done === done ? not done : done
+                for (let i=0; i<arr1.length; i++) {
+                    let index = storeTasks.map(e => e.title).indexOf(arr1[i].title);
+                    for (let j=0; j<storeTasks.length; j++) {
+                        if (j===index) {
+                            storeTasks[j].done === "not done" ? storeTasks[j].done = "done" : storeTasks[j].done = "not done"; 
+                            console.log(storeTasks[j].done);
+                        }
+                    }
+                }
+                
+            }
+            arrayToDisplay(x);
+        }
+    });
 }
 
 export default checkOffTasks;
--- src/sortByDate.js
+++ src/sortByDate.js
@@ -3,6 +3,8 @@ import storeDatedTasks from "./storeDatedTasks";
 
 function sortByDate() {
 
+    storeDatedTasks.length = 0;
+
     let arr1 = storeTasks.map(obj => {
         return {...obj, dueDate: new Date(obj.dueDate)};
     });
@@ -10,8 +12,25 @@ function sortByDate() {
     arr1.sort((a, b) => Number(a.dueDate) - Number(b.dueDate));
 
     for (const obj of arr1) {
-        storeDatedTasks.push((obj));
+        storeDatedTasks.push(obj);
     }
+
+    // so that task deletion is registered in home view array too:
+    
+    document.addEventListener("click", function(e) {
+        const target = e.target.closest(".deleteTaskBtn");
+        if(target){
+
+            let arr2 = storeTasks.filter(({ title }) => storeDatedTasks.some((e) => e.title === title));
+
+            storeTasks.length = 0;
+    
+            for (const obj of arr2) {
+                storeTasks.push(obj);
+            };
+        }
+    });
+
 }
 
 export default sortByDate;
